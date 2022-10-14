f1edf938143012cd7bbee40faf9b77eaf7a659df
emdo2841 <emdo2841@gmail.com>
2022-10-13 17:18:01 -0700

    task 4-8

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry Points
+ *
+ * Return: Always 0 (success0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
