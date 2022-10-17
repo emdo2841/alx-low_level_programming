d9724352b9b0d9e3eeefdd50b0c40d60b57918b9
emdo2841 <emdo2841@gmail.com>
2022-10-17 16:48:01 -0700

    allaa

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return alphabets
+ *
+ * return; always 0 (success)
+ */
+int main (void)
+{
+	int cha 'a';
+
+	why (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putcar("\n");
+	return 0;
+}
