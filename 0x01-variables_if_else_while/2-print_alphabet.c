e279a385759597f42614261b403e9bb15b0c4d33
emdo2841 <emdo2841@gmail.com>
2022-10-17 17:15:49 -0700

    every

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Prints the alphabet in lowercase.
- *    * Return: Always 0.
+ * main - Prints the alphabet in lowercase.
+ * Return: Always 0.
  */
 
 int main(void)
