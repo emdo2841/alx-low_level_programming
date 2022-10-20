cce40b609cfb2c1c2bc4084323a516bdb6e9a9b0
emdo2841 <emdo2841@gmail.com>
2022-10-20 16:01:30 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+fndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int print_remainig_days(int month, int day, int year);
+int convert days(int month, int day);
+
+#endif
