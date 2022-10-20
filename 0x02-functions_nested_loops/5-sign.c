f808e1f4b4cf6196fce73e9a1d5f381e9b886c4b
emdo2841 <emdo2841@gmail.com>
2022-10-19 18:51:56 -0700

    msmsms

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/** 
+ * print_sign-prit the sign
+ * @n; the number to be checked for
+ * Returns 1 and prints + if n is greater than zero
+ 0 and prints 0 if n is zer0 -1 and prints - if n is less than zero
+ * 
+ */
+
+int print_sign (int n)
+{
+	if (n > 0)
+	{
+		_putchar('+");
+	        return (1);
+	}
+	else if (n == )
+	{
+		_putchar('+');
+		return (1);
+	}
+	else 
+	{
+		_putchar('+");
+                return (-1);
+}
