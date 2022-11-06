c9c214b96a790bfd6d4be8b0cb9037644a683a98
emdo2841 <emdo2841@gmail.com>
2022-11-05 23:34:40 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int argc, char ** argv)
+int main(int argc, char argv)
 {
-    printf("%s", argv[0]);
-    return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
