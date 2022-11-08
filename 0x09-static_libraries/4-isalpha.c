391b516814465226462b62314eddac3b9298855e
emdo2841 <emdo2841@gmail.com>
2022-11-07 16:50:38 -0800

    more functions and loops

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check if the char is lowercase.
+ * @c: the char to be checked @_islower: the best
+ * 
+ * Return: 1 if the chair is lowercase, otheerwise 0.
+ */
+
+int _4-isalpha.c(int c)
+{
+        int _islower
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+        else
+                return (0)
+}
