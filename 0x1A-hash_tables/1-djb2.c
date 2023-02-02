249af757608dface3e4b6ae6b0acdde123b4cfd7
emmanuel jonathan <emdo2841@gmail.com>
2023-02-02 10:15:18 -0800

    header file

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,12 +9,12 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
-	int count;
+        unsigned long int hash;
+        int count;
 
-	hash = 5381;
-	while ((count = *str++))
-		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
+        hash = 5381;
+        while ((count = *str++))
+                hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
 
-	return (hash);
+        return (hash);
 }
